5732d287eda98795cea5222629519c01adaa5da6
younheesong <thd6517@naver.com>
2023-08-23 12:18:15 +0900

    fix: setting file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
+  parser: 'babel-parser',
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'error',
+  },
 };
